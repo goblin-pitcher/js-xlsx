b31e8412091c0a8246ee797f2e87ba81741e158c
xiao.wei <799343190@qq.com>
2023-04-26 23:50:03 +0800

    fix: solve issues about fail to resolve  ./captable and jszip is not a constructor

--- xlsx.js
+++ xlsx.js
@@ -1342,7 +1342,7 @@ if(typeof JSZip !== 'undefined') jszip = JSZip;
 if (typeof exports !== 'undefined') {
 	if (typeof module !== 'undefined' && module.exports) {
 		if(has_buf && typeof jszip === 'undefined') jszip = require('js'+'zip');
-		if(typeof jszip === 'undefined') jszip = require('./js'+'zip').JSZip;
+		if(typeof jszip === 'undefined') jszip = require('./js'+'zip');
 		_fs = require('f'+'s');
 	}
 }
